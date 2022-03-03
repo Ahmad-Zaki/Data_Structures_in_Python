95d0bade93d00a45ad1fbadb6ab9e8ba1ade3edc
Ahmad-Zaki <ahmadheshamzaki@gmail.com>
2022-03-03 16:37:31 +0200

    Add linkedlist-based queue implementation.

--- Implementations/Queues.py
+++ Implementations/Queues.py
@@ -140,3 +140,85 @@ class Queue:
         '''Remove all elements from the Queue.'''
         self._elements = []
         self._size = 0
+
+
+class QueueLL(Queue):
+    """LinkedList-based implementation of Queue data structure.
+    
+    Parameters
+    ----------
+    capacity: int
+        Determine the maximum amount of elements a Queue can carry. If unspecified, Queue capacity will be limitless.
+        default = None
+
+    vals: iterable
+        a group of elements that are added to the Queue during its construction. If unspecified, an empty Queue is created. If the number of elements in vals exceeds the specified capacity, An assertion error is raised.
+        default = None
+
+    Methods
+    -------
+    empty() -> bool:
+        Check if the queue is empty.
+
+    full() -> bool:
+        Check if the queue is full.
+
+    enqueue(element) -> self:
+        Add an element to the end of the queue.
+
+    dequeue() -> Any:
+        pop the first element in the queue.
+
+    peek() -> Any:
+        Access the first element of the queue.
+
+    delete() -> None:
+        Remove all elements from the Queue.
+    """
+
+    def __init__(self, capacity: int = None, vals: list = None) -> None:
+        self._assert_params(capacity, vals)
+        self._capacity = capacity
+        self._elements = SinglyLL(vals)
+        self._size = len(self._elements)
+
+    def enqueue(self, element: Any):
+        """Add an element to the end of the queue.
+        
+        Parameters
+        ----------
+        element: Any
+            The element that is added to the queue.
+
+        Returns
+        -------
+        self
+        """
+
+        assert not self.full(), "Maximum queue capacity reached, unable to store more elements."
+
+        self._elements.insert(element)
+        self._size += 1
+
+        return self
+
+    def dequeue(self) -> Any:
+        """pop the first element in the queue.
+
+        Returns
+        -------
+        Element: Any
+            The first element in the queue.
+        """
+
+        assert not self.empty(), "Queue is empty."
+
+        removed_element = self.peek()
+        self._elements.pop(0)
+        self._size -= 1
+        return removed_element
+
+    def delete(self) -> None:
+        '''Remove all elements from the Queue.'''
+        self._elements.delete()
+        self._size = 0
